f07a3e754ab0f4070cd0e89e881a61e32b95fcf6
amit <amit.singh.9312687983@gmail.com>
2023-11-29 22:15:10 +0530

    first commit

---
+++ lib/lecture1.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Lecture1 extends StatefulWidget {
+  const Lecture1({super.key});
+
+  @override
+  State<Lecture1> createState() => _Lecture1State();
+}
+
+class _Lecture1State extends State<Lecture1> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
