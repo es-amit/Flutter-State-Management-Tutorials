16f9a7425ccd5676eb97571c808b56cb0015c446
amit <amit.singh.9312687983@gmail.com>
2023-11-30 11:11:08 +0530

    Slider with provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:state_management/provider/count_provider.dart';
+//import 'package:state_management/provider/count_provider.dart';
+import 'package:state_management/provider/exampleoneProvider.dart';
 //import 'package:state_management/lecture1.dart';
 //import 'package:state_management/lecture2.dart';
 //import 'package:state_management/lecture3.dart';
-import 'package:state_management/screen/count_example.dart';
+//import 'package:state_management/screen/count_example.dart';
+import 'package:state_management/screen/exampleone.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,12 +17,17 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => CountProvider(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_)=> ExampleOneProvider()),
+        ChangeNotifierProvider(
+          create: (_)=> CountProvider()),
+      ],
       child: const MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
-        home: CountExample(),
+        home: ExampleOneScreen(),
       ),
     );
   }
